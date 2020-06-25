5f48612dd1520b12ef414847cd1e75a44617c1ce
Md Nayeem <codernayeem898@gmail.com>
2020-06-25 22:59:37 +0600

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 from os import listdir
-from os.path import join
+from os.path import join, sep
 from datetime import datetime
 from random import randint
 
+
+def create_folder(pth):
+    s = ''
+    for i in pth.split(sep):
+        s = join(s, i)
+        try:
+            Path(s).mkdir()
+        except:
+            pass
+
+
+def get_formatted_datetime(format):
+    return datetime.now().strftime(format)
